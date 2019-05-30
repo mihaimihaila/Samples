656a20e87b73dfc1b6aa3329821262da182951a2
Mihai Mihaila <mihai.mihaila@gmail.com>
2019-05-30 15:00:52 +0200

    Refactoring for ControlCenter sample

--- Swift/ControlCenter/Sample1/ViewController.swift
+++ Swift/ControlCenter/Sample1/ViewController.swift
@@ -21,107 +21,72 @@ class ViewController: UIViewController {
         
         view.addSubview(controlCenter)
         
-        let _ = controlCenter.constraint(width: 320, height: 480)
+        _ = controlCenter.constraint(width: 320, height: 480)
         controlCenter.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         controlCenter.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     func buildControlCenter() -> UIView {
-        return UIView.gridLayoutView(items: [ GridItem(buildPanel(),
-                                                       position: Position(rowSpan: 2, columnSpan: 2),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-                                              GridItem(buildButton(),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       column: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 1,
-                                                       column: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 2,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 2,
-                                                       column: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       position: Position(row: 3, columnSpan: 2),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 4,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 4,
-                                                       column: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 5,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildButton(),
-                                                       row: 5,
-                                                       column: 1,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 12),
-                                              GridItem(buildPanel(),
-                                                       position: Position(column: 2, rowSpan: 2, columnSpan: 2),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-                                              
-                                              GridItem(buildButton(),
-                                                       position: Position(row: 2, column: 2, rowSpan: 2),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-                                              
-                                              GridItem(buildButton(),
-                                                       position: Position(row: 2, column: 3, rowSpan: 2, columnSpan: 2),
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-                                              
-                                              GridItem(buildButton(),
-                                                       row: 4,
-                                                       column: 2,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-                                              
-                                              GridItem(buildButton(),
-                                                       row: 4,
-                                                       column: 3,
-                                                       horizontalAlignment: .stretch,
-                                                       verticalAlignment: .stretch,
-                                                       margin: 8),
-            ],
+        var items = [GridItem]()
+        items.append(GridItem(buildPanel(),
+                              position: Position(rowSpan: 2, columnSpan: 2),
+                              margin: 8).stretched())
+        
+        items.append(GridItem(buildButton(),
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              column: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 1,
+                              column: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 2,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 2,
+                              column: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              position: Position(row: 3, columnSpan: 2),
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 4,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 4,
+                              column: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 5,
+                              margin: 12).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 5,
+                              column: 1,
+                              margin: 12).stretched())
+        items.append(GridItem(buildPanel(),
+                              position: Position(column: 2, rowSpan: 2, columnSpan: 2),
+                              margin: 8).stretched())
+        items.append(GridItem(buildButton(),
+                              position: Position(row: 2, column: 2, rowSpan: 2),
+                              margin: 8).stretched())
+        items.append(GridItem(buildButton(),
+                              position: Position(row: 2, column: 3, rowSpan: 2, columnSpan: 2),
+                              margin: 8).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 4,
+                              column: 2,
+                              margin: 8).stretched())
+        items.append(GridItem(buildButton(),
+                              row: 4,
+                              column: 3,
+                              margin: 8).stretched())
+
+        return UIView.gridLayoutView(items: items,
                                      rows: [.fill, .fill, .fill, .fill, .fill, .fill],
                                      columns: [.fill, .fill, .fill, .fill])
     }
